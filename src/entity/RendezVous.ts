00f42315afcfc1ac06f557d208dd8986de7c2092
S0o0 <S0o0@users.noreply.github.com>
2025-07-11 16:20:02 +0200

    erreur dans la liaison entre user et rdv

--- src/entity/RendezVous.ts
+++ src/entity/RendezVous.ts
@@ -1,5 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
-import { User } from "./User";
+import { User } from "./User"; // import normal car ici on doit fournir la classe
 
 @Entity("rdv2")
 export class RendezVous {
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,5 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
-import { RendezVous } from "./RendezVous"
+import { RendezVous } from "./RendezVous"; // import *type-only* pour casser le cycle
 
 @Entity("users")
 export class User {
