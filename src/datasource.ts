6f8b361e87bab20a3ad11a0408642aedb058635f
S0o0 <S0o0@users.noreply.github.com>
2025-07-11 15:47:08 +0200

    connection à la bdd réussie

--- src/datasource.ts
+++ src/datasource.ts
@@ -7,11 +7,11 @@ config();
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: process.env.DB_HOST,
+    host: process.env.DATABASE_HOST,
     port: Number(process.env.DATABASE_PORT),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
     entities: [User],
     synchronize: true,
 });
